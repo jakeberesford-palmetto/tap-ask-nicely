94042f064bd52c3ff8f89ccab32c497274a8a14e
Michael Cooper <macoop2363@gmail.com>
2021-05-19 11:33:09 -0600

    Add method to fetch responses from the Ask Nicely API

--- tap_ask_nicely/client.py
+++ tap_ask_nicely/client.py
@@ -1,5 +1,7 @@
 import json
 import requests
+from datetime import datetime
+from singer.utils import strptime_to_utc
 
 
 class AskNicelyClient:
@@ -7,3 +9,12 @@ class AskNicelyClient:
         self._base_url = base_url
         self._api_key = api_key
         self._client = requests.Session()
+
+    def fetch_responses(
+        self, page: int, page_size: int, start_time_utc: str, end_time_utc: str
+    ) -> dict:
+        start_time_unix = int(strptime_to_utc(start_time_utc).timestamp())
+        end_time_unix = int(strptime_to_utc(end_time_utc).timestamp())
+        url = f"{self._base_url}/api/v1/responses/asc/{page_size}/{page}/{start_time_unix}/json/{end_time_unix}"
+        params = {"X-apikey": self._api_key}
+        return self._client.get(url, params=params).json()
