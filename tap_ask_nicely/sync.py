96bebbd8933c670375fab97eb7f02b50a5e215be
Ryan Allen <rcallen89@gmail.com>
2021-05-19 14:49:33 -0500

    Add Audit Logging and Slack Notification to sync

--- tap_ask_nicely/sync.py
+++ tap_ask_nicely/sync.py
@@ -1,26 +1,38 @@
+from os import pipe
+from requests.models import cookiejar_from_dict
 import singer
 from singer import Transformer, metadata
-
-# The client name needs to be filled in here
-from tap_ask_nicely.client import CLIENT_CLASS_NAME
+from tap_ask_nicely.client import AskNicelyClient
 from tap_ask_nicely.streams import STREAMS
+from tap_ask_nicely.utils import AuditLogs, SlackMessenger
+from datetime import date, datetime
+import time
 
 LOGGER = singer.get_logger()
 
 
 def sync(config, state, catalog):
-    # Any client required PARAMETERS to hit the endpoint
-    client = CLIENT_CLASS_NAME(CLIENT_PARAMETERS_HERE)
+    client = AskNicelyClient(config)
+
+    run_id = int(time.time())
+    pipeline_start = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+    pipeline_start_time = time.perf_counter()
+    stream_comments = []
+    total_records = 0
 
     with Transformer() as transformer:
         for stream in catalog.get_selected_streams(state):
+            batch_start = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+            start_time = time.perf_counter()
+            record_count = 0
+
             tap_stream_id = stream.tap_stream_id
             stream_obj = STREAMS[tap_stream_id](client, state)
-            # replication_key = stream_obj.replication_key
+            replication_key = stream_obj.replication_key
             stream_schema = stream.schema.to_dict()
             stream_metadata = metadata.to_map(stream.metadata)
 
-            LOGGER.info('Staring sync for stream: %s', tap_stream_id)
+            LOGGER.info("Staring sync for stream: %s", tap_stream_id)
 
             state = singer.set_currently_syncing(state, tap_stream_id)
             singer.write_state(state)
@@ -29,23 +41,59 @@ def sync(config, state, catalog):
                 tap_stream_id,
                 stream_schema,
                 stream_obj.key_properties,
-                stream.replication_key
+                stream.replication_key,
             )
 
-            client = CLIENT_CLASS_NAME(CLIENT_PARAMETERS_HERE)
-            for record in stream_obj.sync(CLIENT_PARAMETERS_HERE):
-                transformed_record = transformer.transform(
-                    record, stream_schema, stream_metadata)
-                LOGGER.info(f"Writing record: {transformed_record}")
-                singer.write_record(
-                    tap_stream_id,
-                    transformed_record,
+            try:
+                for record in stream_obj.sync():
+                    transformed_record = transformer.transform(
+                        record, stream_schema, stream_metadata
+                    )
+                    singer.write_record(
+                        tap_stream_id,
+                        transformed_record,
+                    )
+                    record_count += 1
+                    total_records += 1
+
+                if replication_key != "":
+                    state = singer.write_bookmark()(
+                        state, tap_stream_id, replication_key, record[replication_key]
+                    )
+                    singer.write_state(state, tap_stream_id)
+
+                batch_stop = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+                AuditLogs.write_audit_log(
+                    run_id=run_id,
+                    stream_name=tap_stream_id,
+                    batch_start=batch_start,
+                    batch_end=batch_stop,
+                    records_synced=record_count,
+                    run_time=(time.perf_counter() - start_time),
                 )
 
-            # If there is a Bookmark or state based key to store
-            state = singer.clear_bookmark(
-                state, tap_stream_id, BOOKMARK_KEY)
-            singer.write_state(state, tap_stream_id, )
+            except Exception as e:
+                stream_comments.append(f"{tap_stream_id.upper}: {e}")
+                batch_stop = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+                AuditLogs.write_audit_log(
+                    run_id=run_id,
+                    stream_name=tap_stream_id,
+                    batch_start=batch_start,
+                    batch_end=batch_stop,
+                    records_synced=record_count,
+                    run_time=(time.perf_counter() - start_time),
+                    comments=e,
+                )
 
     state = singer.set_currently_syncing(state, None)
     singer.write_state(state)
+
+    # Comment out for local runs
+    if config["slack_notifications"] == True:
+        SlackMessenger.send_message(
+            run_id=run_id,
+            start_time=pipeline_start,
+            run_time=(time.perf_counter() - pipeline_start_time),
+            record_count=total_records,
+            comments='\n'.join(stream_comments),
+        )
