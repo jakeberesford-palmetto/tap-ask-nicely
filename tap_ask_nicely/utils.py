94067941cb9131c5fa7a3a8e8c56064de05deec9
Jordan <jordan.wallace.williams@gmail.com>
2021-05-26 02:00:35 -0600

    Add GmailMessenger()

--- tap_ask_nicely/utils.py
+++ tap_ask_nicely/utils.py
@@ -4,6 +4,9 @@ import logging as LOGGER
 from singer import Transformer, metadata
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Content, Email, Mail
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 ## Env file for testing - Change to Config for Prod
 ## Keep an Env for a single key in the meltano
@@ -165,7 +168,7 @@ class AuditLogs:
         )
 
 
-class EmailMessenger:
+class SendgridMessenger:
     message = Mail(
         from_email='from_email@example.com',
         to_emails='to@example.com',
@@ -180,5 +183,74 @@ class EmailMessenger:
         LOGGER.info(response.status_code)
         LOGGER.info(response.body)
         LOGGER.info(response.headers)
-    except Exception as exception:
-        LOGGER.warning(exception)
+    except Exception as error:
+        LOGGER.warning(error)
+
+
+class GmailMessenger:
+    def __init__(self) -> None:
+        self.sender_email = "info@mashey.com"
+        self.receiver_email = "jordan@mashey.com"
+        self.password = os.getenv('GMAIL_PW')
+        self.message = MIMEMultipart("alternative")
+        # Add conditional logic for a :red_circle: and :yellow_circle: emoji
+        self.message["Subject"] = "Mashey | Data Sync | :large_green_circle:"
+        self.message["From"] = self.sender_email
+        self.message["To"] = self.receiver_email
+
+    def create_message(self, sync_data: dict):
+        # Create the plain-text and HTML version of your message
+        text = f"""\
+        Hi,
+        It's the Mashey team with a data pipeline update.
+
+        run_id
+        stream_name
+        batch_start
+        batch_end
+        records_synced
+        run_time
+        comments
+        """
+
+        html = f"""\
+        <html>
+        <body>
+            <p>Hi,<br>
+            It's the Mashey team with a data pipeline update.<br>
+            <ul>
+                <li>run_id</li>
+                <li>stream_name</li>
+                <li>batch_start</li>
+                <li>batch_end</li>
+                <li>records_synced</li>
+                <li>run_time</li>
+                <li>comments</li>
+            </ul>
+            <a href="http://www.mashey.com">Mashey</a><br>
+            </p>
+        </body>
+        </html>
+        """
+
+        # Turn these into plain/html MIMEText objects
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+        return part1, part2
+
+
+    def send_message(self, sync_overview):
+        part1, part2 = self.create_message(sync_overview)
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        self.message.attach(part1)
+        self.message.attach(part2)
+
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(self.sender_email, self.password)
+            server.sendmail(
+                self.sender_email, self.receiver_email, self.message.as_string()
+            )
