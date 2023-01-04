4b644794c7e49ed1d9bc4cfeec6210abef8b87a1
Jake Beresford <jake.beresford@palmetto.com>
2023-01-04 14:53:03 -0500

    Fix dumb typo in logs
    DP-252

--- tap_ask_nicely/sync.py
+++ tap_ask_nicely/sync.py
@@ -32,7 +32,7 @@ def sync(config, state, catalog):
             stream_schema = stream.schema.to_dict()
             stream_metadata = metadata.to_map(stream.metadata)
 
-            LOGGER.info("Staring sync for stream: %s", tap_stream_id)
+            LOGGER.info("Starting sync for stream: %s", tap_stream_id)
 
             state = singer.set_currently_syncing(state, tap_stream_id)
             singer.write_state(state)
